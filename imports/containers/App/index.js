086b827c054f33d2a710becf0f500ca13e68cf89
Vincent Wang <a36168120@gmail.com>
2019-08-01 17:02:44 -0700

    Setup subscribe methods for api

--- imports/containers/App/index.js
+++ imports/containers/App/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import "./index.css";
-import { withTracker } from "meteor/react-meteor-data";
-
 import "../../startup/accounts-config";
+import { withTracker } from "meteor/react-meteor-data";
+import Reviews from "../../api/reviews";
+import Tables from "../../api/tables";
+import Users from "../../api/users";
+import Restaurants from "../../api/restaurants";
 
 class App extends Component {
   render() {
@@ -14,4 +17,18 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withTracker(() => {
+  Meteor.subscribe("reviews");
+  Meteor.subscribe("tables");
+  Meteor.subscribe("users");
+  Meteor.subscribe("restaurant");
+
+  return {
+    currentUser: Meteor.user(),
+    currentUserId: Meteor.userId(),
+    reviews: Reviews.find({}).fetch(),
+    tables: Tables.find({}).fetch(),
+    users: Users.find({}).fetch(),
+    restaurant: Restaurants.find({}).fetch()
+  };
+})(App);
