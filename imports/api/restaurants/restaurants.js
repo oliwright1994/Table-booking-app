bd20c61f5b57aa94842450cfaf85498f54133085
Oli Wright <oli.wright@blueyonder.co.uk>
2019-08-02 11:58:11 -0700

    Basic api methods

--- imports/api/restaurants/restaurants.js
+++ imports/api/restaurants/restaurants.js
@@ -9,3 +9,34 @@ if (Meteor.isServer) {
     return Restaurants.find({});
   });
 }
+
+Meteor.methods({
+  "restaurants.createRestaurant"(userId) {
+    Restaurants.insert({
+      owner: userId,
+      name: "This restaurant has no name yet",
+      description: "No bio yet",
+      imageurl: "No image added",
+      phone: "",
+      adress: "",
+      email: "",
+      website: ""
+    });
+  },
+  "restaurants.updateProfile"(values, restaurantId) {
+    Restaurants.update(
+      { _id: restaurantId },
+      {
+        $set: {
+          name: values.name,
+          description: values.description,
+          imageurl: values.imageurl,
+          phone: values.phone,
+          adress: values.adress,
+          email: values.email,
+          website: values.website
+        }
+      }
+    );
+  }
+});
--- imports/api/tables/tables.js
+++ imports/api/tables/tables.js
@@ -9,3 +9,47 @@ if (Meteor.isServer) {
     return Tables.find({});
   });
 }
+
+Meteor.methods({
+  "tables.createTable"(values, userId, restaurantId) {
+    Tables.insert({
+      restaurantId: restaurantId,
+      covers: values.covers,
+      discount: values.discount,
+      notes: values.notes,
+      created: new Date(),
+      customers: [],
+      guests: 0,
+      available: true
+    });
+  },
+  "tables.updateBooking"(tableId, userId, numberOfGuests) {
+    Tables.update(
+      {
+        _id: tabledId
+      },
+      {
+        $addToSet: {
+          customers: { customerId: userId, guests: numberOfGuests }
+        },
+        $inc: {
+          guests: numberOfGuests
+        }
+      }
+    );
+  },
+  "tables.setBookingFull"(tabledId) {
+    Tables.update(
+      {
+        _id: tableId
+      },
+      { $set: { available: false } }
+    );
+  },
+  "tables.cancelBooking"(tableId, userId) {
+    Tables.update(
+      { _id: tabledId },
+      { $pull: { customers: { customerId: userID } },
+    );
+  }
+});
