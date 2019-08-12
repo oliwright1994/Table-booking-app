94a728d2a4f3f211273236415cc579185494e384
Vincent Wang <a36168120@gmail.com>
2019-08-12 14:22:23 -0700

    init styles for your booking page

--- imports/containers/pages/YourBookings/YourBookings.js
+++ imports/containers/pages/YourBookings/YourBookings.js
@@ -2,54 +2,90 @@ import React from "react";
 import styles from "./styles";
 import { withStyles } from "@material-ui/core/styles";
 import BookingCard from "../../Component/BookingCard";
+import CurrentIcon from "@material-ui/icons/timer";
+import PastIcon from "@material-ui/icons/timeroff";
 
 const YourBookings = ({ classes, restaurants, tables }) => {
   return (
-    <div>
-      <p>This is the YourBookings page.</p>
-      <p>Current Booking</p>
+    <div className={classes.background}>
+      <div className={classes.container}>
+        <div className={classes.current}>
+          <p className={classes.title}>
+            <CurrentIcon />
+            &nbsp; Current Booking
+          </p>
 
-      {(tables.length !== 0) ?
-
-        tables.map(table => {
-          if (table.customers.find(customer => (customer.customerId == Meteor.userId()))) {
-            const now = new Date();
-            const tableExpiryDate = new Date(table.expireTime);
-            const expired = !!(now > tableExpiryDate);
-            if (table.expireTime && now <= tableExpiryDate && restaurants.length !== 0) {
-              const restaurant = restaurants.find(
-                restaurant => restaurant._id === table.restaurantId
-              );
-              return (
-                <BookingCard key={table._id} restaurant={restaurant} table={table} expired={expired} />
-
-              );
-            }
-
-
-          }
-        }) : null
-      }
-      <p>Past Booking</p>
-      {(tables.length !== 0) ?
-        tables.map(table => {
-          if (table.customers.find(customer => (customer.customerId == Meteor.userId()))) {
-            const now = new Date();
-            const tableExpiryDate = new Date(table.expireTime);
-            if (table.expireTime && now > tableExpiryDate && restaurants.length !== 0) {
-              const restaurant = restaurants.find(
-                restaurant => restaurant._id === table.restaurantId
-              );
-              return (
-                <div>
-                  <BookingCard key={table._id} restaurant={restaurant} table={table} expired={true} />
-                </div>
-              );
-            }
-          }
-        }
-        ) : null
-      }
+          {tables.length !== 0
+            ? tables.map(table => {
+                if (
+                  table.customers.find(
+                    customer => customer.customerId == Meteor.userId()
+                  )
+                ) {
+                  const now = new Date();
+                  const tableExpiryDate = new Date(table.expireTime);
+                  const expired = !!(now > tableExpiryDate);
+                  if (
+                    table.expireTime &&
+                    now <= tableExpiryDate &&
+                    restaurants.length !== 0
+                  ) {
+                    const restaurant = restaurants.find(
+                      restaurant => restaurant._id === table.restaurantId
+                    );
+                    return (
+                      <div className={classes.booking}>
+                        <BookingCard
+                          key={table._id}
+                          restaurant={restaurant}
+                          table={table}
+                          expired={expired}
+                        />
+                      </div>
+                    );
+                  }
+                }
+              })
+            : null}
+        </div>
+        <div className={classes.past}>
+          <p className={classes.title}>
+            <PastIcon />
+            &nbsp; Past Booking
+          </p>
+          {tables.length !== 0
+            ? tables.map(table => {
+                if (
+                  table.customers.find(
+                    customer => customer.customerId == Meteor.userId()
+                  )
+                ) {
+                  const now = new Date();
+                  const tableExpiryDate = new Date(table.expireTime);
+                  if (
+                    table.expireTime &&
+                    now > tableExpiryDate &&
+                    restaurants.length !== 0
+                  ) {
+                    const restaurant = restaurants.find(
+                      restaurant => restaurant._id === table.restaurantId
+                    );
+                    return (
+                      <div className={classes.booking}>
+                        <BookingCard
+                          key={table._id}
+                          restaurant={restaurant}
+                          table={table}
+                          expired={true}
+                        />
+                      </div>
+                    );
+                  }
+                }
+              })
+            : null}
+        </div>
+      </div>
     </div>
   );
 };
--- imports/containers/pages/YourBookings/styles.js
+++ imports/containers/pages/YourBookings/styles.js
@@ -1,3 +1,37 @@
-const styles = theme => ({});
+const styles = theme => ({
+  background: {
+    backgroundImage: "url('/assets/images/boom.jpg')",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center"
+  },
+
+  container: {
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr",
+    gridTemplateAreas: `"current past"`,
+    margin: "50px 100px"
+  },
+
+  current: {
+    gridArea: "current",
+    justifySelf: "center"
+  },
+
+  past: {
+    gridArea: "past",
+    justifySelf: "center"
+  },
+
+  title: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  booking: {
+    marginBottom: "50px"
+  }
+});
 
 export default styles;
